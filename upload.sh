9d065d99042b2bad873a3a1aec80016e0535ecac
Glen Campbell <glen@broadpool.com>
2015-07-16 22:35:03 -0700

    fixed to use sync

--- upload.sh
+++ upload.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 FS=$(echo -en "\n\b")
 cd files.glencampbell.co
-for file in *; do
-  echo Uploading $file...
-  aws s3 cp "$file" s3://files.glencampbell.co --cache-control max-age=31536000
-done
+aws s3 sync . s3://files.glencampbell.co --cache-control max-age=31536000
